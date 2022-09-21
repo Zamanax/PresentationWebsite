5f00d4329c12f92097950c79873c10472595bce1
Zamanax <zamanimaxi@cy-tech.fr>
2022-09-21 22:45:25 +0200

    Implemented navigation and a first test page

---
+++ src/English.jsx
@@ -0,0 +1,24 @@
+import "./App.css"
+
+function English() {
+    return (
+        <div className="App">
+            <h1>English</h1>
+
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                tincidunt, nisl eget aliquam tincidunt, nisl nisl aliquam
+                tortor, eget aliquam nisl nisl sit amet nisl. Sed tincidunt
+                tincidunt nisl, eget aliquam nisl nisl sit amet nisl. Sed
+                tincidunt, nisl eget aliquam tincidunt, nisl nisl aliquam
+                tortor, eget aliquam nisl nisl sit amet nisl. Sed tincidunt
+                tincidunt nisl, eget aliquam nisl nisl sit amet nisl. Sed
+                tincidunt, nisl eget aliquam tincidunt, nisl nisl aliquam
+                tortor, eget aliquam nisl nisl sit amet nisl. Sed tincidunt
+            
+            </p>
+        </div>
+    )
+}
+
+export default English;
